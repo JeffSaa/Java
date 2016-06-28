3ef4836c0aaee6c94a8f082dc64ed807f3ae24ca
InvitadoAugusto <InvitadoAugusto>
2016-06-28 11:06:17 -0500

    Getter and setter

--- src/javatutorial/JavaTutorial.java
+++ src/javatutorial/JavaTutorial.java
@@ -1,30 +1,37 @@
 package javatutorial;
 
 import java.util.Scanner;
-
-import model.Car; // Import the class, it is in other package
+import model.Car;
 
 public class JavaTutorial {
     
     public static void main(String[] args) {
-        
         Scanner read = new Scanner(System.in);
         
-        // Ask for the data
-        System.out.print("Owner here: ");
+        Car car = new Car("Jeff","abc123",2016,10000); // First create a car
+        
+        System.out.print("New owner here: ");
         String owner = read.nextLine();
-        System.out.print("Id here: ");
+        System.out.print("New id here: ");
         String id = read.nextLine();
-        System.out.print("Years here: ");
-        int years = read.nextInt();
-        System.out.print("Km here: ");
-        float km = read.nextFloat(); // Decimal in Java is with <<,>>
+        System.out.print("New years here: ");
+        int year = read.nextInt();
+        System.out.print("New km here: ");
+        float km = read.nextFloat();
         
-        // Create the car
-        Car first_object = new Car(owner,id,years,km);
+        // Now change the values
+        car.setId(id);
+        car.setOwner(owner);
+        car.setKm(km);
+        car.setYear(year);
         
-        System.out.println("You have a new car!!!");
-    
-    }    
+        // Write new data
+        System.out.println("----You new car's data----");
+        System.out.println("Owner: "+car.getOwner());
+        System.out.println("Id: "+car.getId());
+        System.out.println("Year: "+car.getYear());
+        System.out.println("Km: "+car.getKm());
+       
+    }
     
 }
