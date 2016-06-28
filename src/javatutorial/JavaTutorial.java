66c9c82a2db9a65ec4a66c5adfc2c89704a97396
InvitadoAugusto <InvitadoAugusto>
2016-06-28 11:21:03 -0500

    Jframe

--- src/javatutorial/JavaTutorial.java
+++ src/javatutorial/JavaTutorial.java
@@ -1,9 +1,12 @@
 package javatutorial;
 
+import views.*;
+
 public class JavaTutorial {
     
     public static void main(String[] args) {
-        
+        View1 frame = new View1();
+        frame.setVisible(true);
     }
     
 }
