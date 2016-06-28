ab2d5b493b178fccd5759191adfa672996de53b3
InvitadoAugusto <InvitadoAugusto>
2016-06-28 10:59:38 -0500

    Files

--- src/javatutorial/JavaTutorial.java
+++ src/javatutorial/JavaTutorial.java
@@ -1,23 +1,67 @@
 package javatutorial;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class JavaTutorial {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
-        sayHello(); // Call a subroutine
-        
-        int result = calSum(5,3); // Call a function
-        System.out.println(result);
+        writeFile();
+        readFile();
     
     }
     
-    public static void sayHello(){
-        System.out.println("Hello!!!");
+    public static void writeFile() throws IOException{
+        
+        // Create a file
+        File f;
+        f = new File("MyFile.txt");
+        
+        // Create necesary objects
+        FileWriter fr = new FileWriter(f,true); // True value means dont will erase file's previous content
+        BufferedWriter bw = new BufferedWriter(fr);
+        PrintWriter pw = new PrintWriter(bw);
+        
+        // Write
+        pw.print("Same ");
+        pw.print("line!!!");
+        bw.newLine();
+        pw.println("Hi there!");
+        
+        // Close
+        pw.close();
+        bw.close();
+        fr.close();
+        
     }
     
-    public static int calSum(int x, int y){
-        int r = x+y;
-        return r; // Return a value
+    public static void readFile() throws FileNotFoundException, IOException{
+        
+        // Create a file
+        File f;
+        f = new File("MyFile.txt");
+        
+        // Create necesary objects
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
+        
+        // In 'line' we save a file's line and write in console
+        String line;        
+        while (br.ready()) {
+            line = br.readLine();
+             System.out.println(line);
+        }
+        
+        // Close all
+         br.close();
+         fr.close();
     }
     
 }
