cf4fe1f17244ddd621d51e2987e7dd85bfb9022d
InvitadoAugusto <InvitadoAugusto>
2016-06-28 10:26:32 -0500

    Writing in console

--- src/javatutorial/JavaTutorial.java
+++ src/javatutorial/JavaTutorial.java
@@ -4,6 +4,26 @@ public class JavaTutorial {
     
     public static void main(String[] args) {
         
+        // With 'print' we can write in console in the same line
+        System.out.print("Is ");
+        System.out.print("the ");
+        System.out.print("line!!!");
+        
+        // Here write empry lines
+        System.out.println("");
+        System.out.println("");
+        
+        // With 'println' write in different line
+        System.out.println("Other");
+        System.out.println("Line!");
+        
+        System.out.println("");
+        System.out.println("");
+        
+        // Concatenate
+        String name = "jeff";
+        System.out.println("Hello "+name);
+        
     }
     
 }
